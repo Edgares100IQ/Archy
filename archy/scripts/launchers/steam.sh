cac2481f750ea7ddca2d920039db4e91a07f8c9e
Edgares100IQ <edgar.08ds@gmail.com>
2026-04-18 10:46:11 +0200

    Update steam.sh

--- archy/scripts/launchers/steam.sh
+++ archy/scripts/launchers/steam.sh
@@ -9,6 +9,7 @@ trap 'sudo rm -f /etc/sudoers.d/99-nopasswd-temp; kill $SUDO_PID 2>/dev/null' EX
 
 echo ">>> Instalando Steam..."
 sudo pacman -S --needed flatpak --noconfirm
+sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 flatpak install flathub com.valvesoftware.Steam -y
 
 echo ">>> Steam instalado"
