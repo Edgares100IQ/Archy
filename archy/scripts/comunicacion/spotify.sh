44f1d41a4640dd985b3815098ac1b62af010bd41
Edgares100IQ <edgar.08ds@gmail.com>
2026-05-23 17:40:25 +0200

    Update spotify.sh

--- archy/scripts/comunicacion/spotify.sh
+++ archy/scripts/comunicacion/spotify.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo ">>> Instalando Spotify..."
-yay -S --noconfirm --needed --answerclean All --answerdiff None spotify
+yay -S --noconfirm --needed --answerclean All --answerdiff None antigravity-hub-bin
 echo ">>> Spotify instalado"
