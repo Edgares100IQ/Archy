693134c6015d9104b779dbeee3c4961456b35918
Edgares100IQ <edgar.08ds@gmail.com>
2026-05-23 17:41:07 +0200

    Update tlauncher.sh

--- archy/scripts/juegos/tlauncher.sh
+++ archy/scripts/juegos/tlauncher.sh
@@ -4,6 +4,5 @@ echo ">>> Instalando dependencias (Java)..."
 sudo pacman -S --needed jre-openjdk --noconfirm
 
 echo ">>> Instalando TLauncher..."
-yay -S --noconfirm --needed --answerclean All --answerdiff None tlauncher
-
+yay -S --noconfirm --needed --answerclean All --answerdiff None tlauncher-bin
 echo ">>> TLauncher instalado"
